b9bbfd36f5860532a87ece12e47a6283f51b3eec
Alec Scully <ScullyAlec@gmail.com>
2021-01-19 22:46:20 -0500

    commit tree fbc21f69ade75e33d0bc55f73c512defd87c2787

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+// let formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// };
+
+// // method: "POST" is missing from the object below
+// let configObj = {
+//     method: "POST"
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+// };
+  
+// fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Bad things! Ragnarők!");
+//         console.log(error.message);
+// }); 
+
+function submitData(username, email) {
+    let url = "http://localhost:3000/users"
+    let object = {
+        name: username,
+        email: email
+    }
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body: JSON.stringify(object)
+    };
+
+    return fetch(url, configObj)
+        .then(res => res.json())
+        .then(function(object) {
+            console.log(object.id)
+            document.body.innerHTML = object.id
+        })
+        .catch(function(error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message
+        })
+}
